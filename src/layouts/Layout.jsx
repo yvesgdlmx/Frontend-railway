90567d31633c0cbd4be17e36ea0a92ed8c4b4443
yvesgdlmx <gerencia.tecnologias@optimex.mx>
2024-12-09 10:46:24 -0600

    Agregando antiguedad de trabajos

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -186,6 +186,11 @@ const Layout = () => {
                                                 <p className="block px-4 py-2 hover:bg-gray-100">Reporte de trabajos nuevos</p>
                                             </Link>
                                         </li>
+                                        <li>
+                                            <Link to={'/reportes_antiguedad'} onClick={handleMenuItemClick}>
+                                                <p className="block px-4 py-2 hover:bg-gray-100">Reporte de Antigüedad</p>
+                                            </Link>
+                                        </li>
                                     </ul>
                                 </div>
                             )}
@@ -286,6 +291,15 @@ const Layout = () => {
                                                 className="block py-2 px-3 text-gray-600 rounded-lg hover:bg-gray-100 transition-colors"
                                             >
                                                 Reportes trabajos nuevos
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link 
+                                                to={'/reportes_antiguedad'} 
+                                                onClick={handleMenuItemClick} 
+                                                className="block py-2 px-3 text-gray-600 rounded-lg hover:bg-gray-100 transition-colors"
+                                            >
+                                                Reporte de Antigüedad
                                             </Link>
                                         </li>
                                     </ul>
