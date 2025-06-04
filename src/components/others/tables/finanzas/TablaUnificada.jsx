deb426d2695af8fe7bf5520f0ac24977836ffa05
yvesgdlmx <gerencia.tecnologias@optimex.mx>
2025-06-04 11:04:04 -0600

    Agregando secciones de finanzas

--- src/components/others/tables/finanzas/TablaUnificada.jsx
+++ src/components/others/tables/finanzas/TablaUnificada.jsx
@@ -7,6 +7,7 @@ import { mergeRegistros } from "../../../../../helpers/margeFinanzas";
 import { calculateTotales } from "../../../../../helpers/calculateTotales";
 import { columnsFinanzas } from "../../../../../config/columnsFinanzas";
 import FinanzasWeek from "../../../exportacionesXLSX/FinanzasWeek";
+
 const TablaUnificada = ({ anio, semana }) => {
   const nviData = useFetchNviWeek(anio, semana);
   const hoyaData = useFetchHoyaWeek(anio, semana);
